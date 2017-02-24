f1c69aacf6da07a0a4083f2e88e634733349d9ba
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-24 16:15:23 -0500

    remove dumb include

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -27,8 +27,6 @@
 
 
       <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
-
-      <script src="{{ URL::asset('js/app.js') }}"></script>
       <script src="{{URL::asset('js/jquery.min.js')}}"></script>
 
     <!-- Scripts -->
