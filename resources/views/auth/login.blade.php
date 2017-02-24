7f4d264955ce5e5c5bcf19e850e5657d2143bdb5
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-24 15:48:43 -0500

    login page test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-3">
+                                <div class="col-md-8 col-md-offset-3 col-sm-8 col-sm-offset-3">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
