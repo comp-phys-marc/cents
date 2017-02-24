f7d42d3fe3bcb54049ae56fcf7bfa7620a07f996
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-24 15:36:19 -0500

    login page test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                                     <label for="email" class="col-md-6 control-label">E-Mail Address</label>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-10">
+                                    <div class="col-md-12">
                                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                         @if ($errors->has('email'))
@@ -47,7 +47,7 @@
                                     <label for="password" class="col-md-6 control-label">Password</label>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-10">
+                                    <div class="col-md-12">
                                         <input id="password" type="password" class="form-control" name="password" required>
 
                                         @if ($errors->has('password'))
@@ -59,7 +59,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-md-10 col-md-offset-4">
+                                <div class="col-md-12 col-md-offset-4">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
