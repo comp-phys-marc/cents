b565af6300b2ebd0a7b7544c394e32c3a1d2d809
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-24 15:30:52 -0500

    login page test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 
 
-    <div class="container app-container app-login">
+    <div class="app-container app-login">
         <div class="flex-center">
             <div class="app-header"></div>
             <div class="app-body">
@@ -27,29 +27,35 @@
                         <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                             {{ csrf_field() }}
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                <div class="row">
+                                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <div class="row">
+                                    <label for="password" class="col-md-4 control-label">Password</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control" name="password" required>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group">
