b2e03c16328873cee88bceeafc11730461bfc92a
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-24 16:16:56 -0500

    fix dumb include

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -27,7 +27,7 @@
 
 
       <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
-      <script src="{{URL::asset('js/jquery.min.js')}}"></script>
+      <script src="{{URL::asset('js/jquery-3.1.1.min.js')}}"></script>
 
     <!-- Scripts -->
     <script>
