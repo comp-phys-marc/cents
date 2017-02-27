df135a16ed6bd882a9d2fa7c4a8bf43c51787d55
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-27 12:43:27 -0500

    auth links

--- resources/views/auth/authnavbar.blade.php
+++ resources/views/auth/authnavbar.blade.php
@@ -3,15 +3,19 @@
         <div class="navbar-header" style="width:100%;">
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="#">
-                <b>Cents</b>
-            </a>
+            <div class="col-md-3 col-sm-3">
+                <a class="navbar-brand" href="#">
+                    <b>Cents</b>
+                </a>
+            </div>
             <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right navbar-links">
-                <!-- Authentication Links -->
-                <li><a href="{{ url('/login') }}">Login</a></li>
-                <li><a href="{{ url('/register') }}">Register</a></li>
-            </ul>
+            <div class="col-md-9 col-sm-9">
+                <ul class="nav navbar-nav navbar-right navbar-links">
+                    <!-- Authentication Links -->
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
