50dbe0cd21b2f214dbc7afd8d776c2bc4b241c52
Marcus Edwards <marcusedwards@hotmail.ca>
2017-02-27 15:44:26 -0500

    buttons

--- resources/views/auth/authnavbar.blade.php
+++ resources/views/auth/authnavbar.blade.php
@@ -6,8 +6,8 @@
             <div class="col-md-4 col-sm-4 col-xs-4 col-xs-offset-8 col-md-offset-8 col-sm-offset-8">
                 <ul class="nav navbar-nav navbar-right navbar-links">
                     <!-- Authentication Links -->
-                    <li><a class="btn btn-primary" href="{{ url('/login') }}">Login</a></li>
-                    <li><a class="btn btn-primary" href="{{ url('/register') }}">Register</a></li>
+                    <li><a class="btn btn-primary nav-button" href="{{ url('/login') }}">Login</a></li>
+                    <li><a class="btn btn-primary nav-button" href="{{ url('/register') }}">Register</a></li>
                 </ul>
             </div>
         </div>
