fd38560b8294202e1c894f5fa04dbab933dada42
Zach <zach.packull@gmail.com>
2017-02-27 19:59:14 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2>Campaigns</h2>
-                    <div class="btn btn-default text-right">+</div>
+                    <h2>Campaigns
+                        <div class="btn btn-default text-right">+</div>
+                    </h2>
                 </div>
 
                 <div class="panel-body">
