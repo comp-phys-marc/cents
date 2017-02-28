913255a05a24896b629e85e30d199377b5ad855b
Zach <zach.packull@gmail.com>
2017-02-27 20:04:27 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h2>Campaigns
@@ -15,7 +15,7 @@
 
                 </div>
             </div>
-
+        </div>
     </div>
 </div>
 @endsection
