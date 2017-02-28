5ec3ca31246e5102c73f75b2dcfd5d62bb23c270
Zach <zach.packull@gmail.com>
2017-02-27 19:47:56 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Campaigns
+                    <div class="btn btn-default">+</div>
+                </div>
 
                 <div class="panel-body">
-                    You are logged in!
+
                 </div>
             </div>
         </div>
