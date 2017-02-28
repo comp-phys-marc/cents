82614af413bfd9d1dc23833eed51d4db79e01ccc
Zach <zach.packull@gmail.com>
2017-02-27 20:09:20 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,31 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3>Campaigns
+                    <h3>My Campaigns
                         <div class="btn btn-default text-right">+</div>
                     </h3>
                 </div>
 
                 <div class="panel-body">
+                    <table>
 
+                    </table>
                 </div>
             </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3>Campaigns</h3>
+                </div>
+
+                <div class="panel-body">
+                    <table>
+
+                    </table>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
