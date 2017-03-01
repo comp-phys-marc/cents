574f10b9b3c525292c622b1e627e6fb16f7d2b5f
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-01 16:47:28 -0500

    card thing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,20 +36,38 @@
                     </div>
                 </div>
             </div>
-
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3>Campaigns</h3>
-                </div>
-
-                <div class="panel-body">
-                    <table>
-
-                    </table>
+                <div class="card card-mini">
+                    <div class="card-header">
+                        <div class="card-title">All Campaigns</div>
+                        <ul class="card-action">
+                            <li>
+                                <a href="/">
+                                    <div class="btn btn-default pull-right">+</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body no-padding table-responsive">
+                        <table class="table card-table">
+                            <thead>
+                            <tr>
+                                <th>Products</th>
+                                <th class="right">Amount</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>MicroSD 64Gb</td>
+                                <td class="right">12</td>
+                                <td><span class="badge badge-success badge-icon"><i class="fa fa-check" aria-hidden="true"></i><span>Complete</span></span></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-
-
         </div>
     </div>
 </div>
