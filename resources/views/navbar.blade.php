dd634a3ca98ff372ad67d39b4f94b3eb735861e0
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-01 19:57:55 -0500

    navbar debug

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -26,47 +26,48 @@
             </ul>
             <ul class="nav navbar-nav navbar-left">
                 <li class="navbar-title">Cents App</li>
+                <li class="navbar-search hidden-sm">
+                    <input id="search" type="text" placeholder="Search..">
+                    <button class="btn-search"><i class="fa fa-search"></i></button>
+                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li class="logo">
-                    <a class="navbar-brand" href="#"><span class="highlight">Cents</span> App</a>
-                </li>
-                <li>
+                <li class="dropdown profile">
                     <div class="btn-group nav-button-right" role="group">
                         <a type="button" class="dropdown profile btn nav-button">
-                            <div class="dropdown-menu">
-                                <div class="profile-info">
-                                    <h4 class="username">Scott White</h4>
-                                </div>
-                                <ul class="action">
-                                    <li>
-                                        <a href="#">
-                                            Profile
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <span class="badge badge-danger pull-right">5</span>
-                                            My Inbox
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            Setting
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            Logout
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
                             <span class="fa fa-user-circle user-circle"></span>
                         </a>
                         <a type="button" class="btn btn-success nav-button" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             Logout
                         </a>
+                    </div>
+                    <div class="dropdown-menu">
+                        <div class="profile-info">
+                            <h4 class="username">Scott White</h4>
+                        </div>
+                        <ul class="action">
+                            <li>
+                                <a href="#">
+                                    Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="badge badge-danger pull-right">5</span>
+                                    My Inbox
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Setting
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </li>
             </ul>
