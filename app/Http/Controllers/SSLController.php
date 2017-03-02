073013f6b5c8206c06a16150f2a3c74e016eccae
Zach <zach.packull@gmail.com>
2017-03-02 12:17:54 -0500

    SSL route

---
+++ app/Http/Controllers/SSLController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SSLController extends Controller
+{
+
+
+    public function File1()
+    {
+        return response()->file('.well-known/acme-challenege/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I');
+    }
+
+    public function File2()
+    {
+        return response()->file('.well-known/acme-challenege/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,8 @@ Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 //social
 
 Route::post('/social/login', 'UserController@socialLogin')->name('social_login');
+
+
+Route::get('/.well-known/acme-challenge/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I', 'SSLController@File1')->name('SSL1');
+
+Route::get('/.well-known/acme-challenge/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A', 'SSLController@File2')->name('SSL2');
