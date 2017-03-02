493f0abae18ebe6a90673c868f8e5843444cd1ce
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-02 14:05:19 -0500

    get rid of silly merge message

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -19,8 +19,6 @@
                     </div>
                 </li>
             </ul>
-
-<<<<<<< HEAD
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right brand">
                 <!-- Authentication Links -->
@@ -28,26 +26,19 @@
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
                 @else
+                    <div class="btn-group nav-button-right" role="group" >
+                        <a type="button" class="btn nav-button">
+                            <span class="fa fa-user-circle user-circle" ></span>
+                        </a>
 
-
-
-                            <div class="btn-group nav-button-right" role="group" >
-                                <a type="button" class="btn nav-button">
-                                    <span class="fa fa-user-circle user-circle" ></span>
-                                </a>
-
-                                    <a type="button" class="btn btn-success nav-button" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-                            </div>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-
+                        <a type="button" class="btn btn-success nav-button" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                    </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 @endif
-
             <ul class="nav navbar-nav navbar-left">
                 <li class="logo navbar-header">
                     <a class="navbar-brand" href="#"><span class="highlight">Cents</span> App</a>
