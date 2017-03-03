77280c7494da123945a6dfc09a42952c959b9780
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-02 19:04:31 -0500

    play with ssl

--- app/Http/Controllers/SSLController.php
+++ app/Http/Controllers/SSLController.php
@@ -2,19 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SSLController extends Controller
 {
 
 
     public function File1()
     {
-        return response()->file(public_path().'/well-known/acme-challenge/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I');
+        return response()->file(base_path().'/etc/ssl/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I');
     }
 
     public function File2()
     {
-        return response()->file(asset('well-known/acme-challenge/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A'));
+        return response()->file(base_path().'/etc/ssl/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A');
     }
 }
