fa93c644607fa7373179ddf39cf7abf0c6eb99f6
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-02 19:38:08 -0500

    debug ssl

--- app/Http/Controllers/SSLController.php
+++ app/Http/Controllers/SSLController.php
@@ -10,13 +10,13 @@ class SSLController extends Controller
 
     public function File1()
     {
-        $file = File::get(base_path().'/etc/ssl/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I');
+        $file = File::get(public_path().'/etc/ssl/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I');
         return $file;
     }
 
     public function File2()
     {
-        $file = File::get(base_path().'/etc/ssl/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A');
+        $file = File::get(public_path().'/etc/ssl/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A');
         return $file;
     }
 }
