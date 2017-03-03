c0a99c4a2d39b5fed907883634ba3a066532253a
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-02 20:15:57 -0500

    remove silly ssl

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,3 @@ Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 //social
 
 Route::post('/social/login', 'UserController@socialLogin')->name('social_login');
-
-//SSL
-
-Route::get('/{well-known}/acme-challenge/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I', 'SSLController@File1')->where('well-known', '/[^\/]+/')->name('SSL1');
-
-Route::get('/{well-known}/acme-challenge/mvjuKMRHhsfYoqxJwpVAvnYnH4mEoPiL06dLo2Epr1A', 'SSLController@File2')->name('SSL2');
-
-//ssl debug
-
-Route::get('/well-known/acme-challenge/CcdxAVaZBHMCmFkWLTKHEXTv_6ggh30ymzOrihprM_I', 'SSLController@File1')->name('SSL1');
