cfc27654e30dd56abfddcd860025b9ca72b27644
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-06 13:00:24 -0500

    button placement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -237,9 +237,9 @@
                             </div>
                             <div class="modal-footer">
                                 <div class="row">
-                                    <button id="{{ $campaign->id }}" type="button" class="btn btn-info close-campaign-button pull-left">Wrap it up!</button>
-                                    <button id="{{ $campaign->id }}" type="button" class="btn btn-danger delete-campaign-button">Delete</button>
+                                    <button id="{{ $campaign->id }}" type="button" class="btn btn-info close-campaign-button">Wrap it up!</button>
                                     <a href="" id="modal-link-button"><button type="submit" class="btn btn-success">Submit</button></a>
+                                    <button id="{{ $campaign->id }}" type="button" class="btn btn-danger delete-campaign-button">Delete</button>
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                                 </div>
                             </div>
