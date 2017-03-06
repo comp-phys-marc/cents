b9c62a90b4db897dff24734585676090a49f95cc
Marcus Edwards <marcusedwards@hotmail.ca>
2017-03-06 09:56:13 -0500

    precision on money inputs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,6 +79,22 @@
                     <div id="loader-title" class="title">Loading content...</div>
                 </div>
                 <div class="app-block">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <div class="flash-message">
+                        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                            @if(session('alert-' . $msg))
+                                <p class="alert alert-{{ $msg }}">{{ session('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                            @endif
+                        @endforeach
+                    </div>
                     <div class="app-form">
                         <div class="row">
                             <div id="mobile-links" class="text-center">
