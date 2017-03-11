c0381e43e52eba6dde97a125a1f46d5cafb27c36
Zach <zach.packull@gmail.com>
2017-03-11 17:12:19 -0500

    stripe test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -290,6 +290,15 @@
                         <div class="modal-body">
                             <p>Magical Campaign View here</p>
                             <button class = "btn btn-success" href="config.php">Pay</button>
+                            <?php require_once('./config.php'); ?>
+
+                            <form action="charge.php" method="post">
+                                <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                                        data-key="<?php echo $stripe['publishable_key']; ?>"
+                                        data-description="Access for a year"
+                                        data-amount="5000"
+                                        data-locale="auto"></script>
+                            </form>
                         </div>
                     </div>
                 </div>
