a2fbdf2f041a1f9853515e87e5c4d93f0f42e70b
Zach <zach.packull@gmail.com>
2017-03-11 16:50:31 -0500

    stripe install

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -50,14 +50,5 @@
             $('.loader-container').show();
         });
     </script>
-    <?php
-    require_once('vendor/autoload.php');
 
-    $stripe = array(
-        "secret_key"      => "sk_test_ujuINGdAEzOVpUT3bfUQltdL",
-        "publishable_key" => "pk_test_wfR5LNQXkcnvYxnQHjtDd5ox"
-    );
-
-    \Stripe\Stripe::setApiKey($stripe['secret_key']);
-    ?>
 </head>
