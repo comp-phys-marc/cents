f2fac903728f9397cbe229538a2b0020447f9500
Zach <zach.packull@gmail.com>
2017-03-14 16:39:49 -0400

    stripe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -282,7 +282,7 @@
                         <div class="modal-body">
                             <p>Magical Campaign View here</p>
 
-
+                            <button class = "btn btn-success" id="customButton">Purchase</button>
                             <form action="./charge.php" stlye= "" method="POST">
                                 <script
                                         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
@@ -308,6 +308,33 @@
 @section('footer')
     <script>
         $(document).ready(function() {
+
+
+            var handler = StripeCheckout.configure({
+                key: 'pk_test_wfR5LNQXkcnvYxnQHjtDd5ox',
+                image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+                locale: 'auto',
+                token: function(token) {
+                    // You can access the token ID with `token.id`.
+                    // Get the token ID to your server-side code for use.
+                }
+            });
+
+            document.getElementById('customButton').addEventListener('click', function(e) {
+                // Open Checkout with further options:
+                handler.open({
+                    name: 'Cents',
+                    description: '2 widgets',
+                    zipCode: true,
+                    currency: 'cad',
+                    amount: 2000
+                });
+                e.preventDefault();
+            });
+
+            window.addEventListener('popstate', function() {
+                handler.close();
+            });
 
             new Clipboard('.clip-button');
 
