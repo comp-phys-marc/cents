c1f6624f43ad6da9cbfe44324bf5d39a19812482
Marcus Edwards <marcusedwards@hotmail.ca>
2017-06-11 16:40:54 -0400

    bank info field sizeing and responsivity

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -127,7 +127,7 @@
                             <br>
 
                             <div class="form-group{{ $errors->has('transit') ? ' has-error' : '' }}">
-                                <label for="address" class="col-md-4 control-label" style="width:215px;">Transit Number</label>
+                                <label for="address" class="col-md-4 control-label bank-label" style="width:215px;">Transit Number</label>
 
                                 <div class="col-md-6 input-group bank-field bank">
                                     <span class="input-group-addon"><i id="transit-show" class="fa fa-eye" aria-hidden="true"></i></span><input id="transit" type="password" class="form-control" name="transit" required>
@@ -135,7 +135,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('institution') ? ' has-error' : '' }}">
-                                <label for="institution" class="col-md-4 control-label" style="width:215px;">Institution Number</label>
+                                <label for="institution" class="col-md-4 control-label bank-label" style="width:215px;">Institution Number</label>
 
                                 <div class="col-md-6 input-group bank-field bank">
                                     <span class="input-group-addon"><i id="institution-show" class="fa fa-eye" aria-hidden="true"></i></span><input id="institution" type="password" class="form-control" name="institution" required>
@@ -143,7 +143,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('account') ? ' has-error' : '' }}">
-                                <label for="account" class="col-md-4 control-label" style="width:215px;">Account Number</label>
+                                <label for="account" class="col-md-4 control-label bank-label" style="width:215px;">Account Number</label>
 
                                 <div class="col-md-6 input-group bank-field bank">
                                     <span class="input-group-addon"><i id="account-show" class="fa fa-eye" aria-hidden="true"></i></span><input id="account" type="password" class="form-control" name="account" required>
@@ -181,6 +181,12 @@
                 $('.container').removeClass('padding-top-2');
                 $('.bank').addClass('bank-field');
             }
+            if($(window).width() < 1080){
+                $('.bank-label').width(215);
+            }
+            else{
+                $('.bank-label').width(240);
+            }
             $(window).resize(function () {
                 if ($(window).width() < 770) {
                     $('.container').addClass('padding-top-2');
@@ -189,6 +195,12 @@
                 else {
                     $('.container').removeClass('padding-top-2');
                     $('.bank').addClass('bank-field');
+                }
+                if($(window).width() < 1080){
+                    $('.bank-label').width(215);
+                }
+                else{
+                    $('.bank-label').width(240);
                 }
             });
         });
